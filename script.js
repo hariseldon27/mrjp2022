01ab744f2cbbef75ff008c7f4281346a1405675f
Jonathan Potter <jonathan@mrjonathanpotter.com>
2021-12-16 09:44:41 -0800

    reformatted dataset and got js dataset counter working

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ h2.textContent = 'This content added by Javascript';
 
 document.querySelector('body').appendChild(h2);
 
-const div = document.createElement('div');
-div.textContent = 'This is a new element';
 
-document.querySelector('li').appendChild(div);
 
 /* Dataset below!  
 
@@ -30,16 +27,6 @@ item1: {
       tag1: "inspirational",
       tag2: "tangential"
     },
-
-    */
-const items = {
-    item1: {
-      type: "image",
-      url: "https://picsum.photos/638/",
-      altText: "A lovely photo of downtown everywhereville.",
-      tag1: "inspirational",
-      tag2: "tangential"
-    },
     item2: {
         type: "poster",
         text: "This is a poster block - whatcha think?",
@@ -54,56 +41,120 @@ const items = {
         tag1: "family",
         tag2: "personal"
       },
-    item4: {
-        type: "image",
-        url: "https://picsum.photos/638/",
-        altText: "A lovely photo of downtown everywhereville.",
-        tag1: "inspirational",
-        tag2: "tangential"
-      },
-    item5: {
-          type: "poster",
-          text: "This is a poster block - whatcha think?",
-          background: "0f4c81",
-          orientation: "landscape",
-          tag1: "wild",
-          tag2: "wisdom"
-        },
-    item6: {
-            type: "image",
-            url: "https://picsum.photos/638/",
-            altText: "A lovely photo of downtown everywhereville.",
-            tag1: "inspirational",
-            tag2: "tangential"
-          },
-    item7: {
-            type: "image",
-            url: "https://picsum.photos/638/",
-            altText: "A lovely photo of downtown everywhereville.",
-            tag1: "inspirational",
-            tag2: "tangential"
-          },
-    item8: {
-            type: "image",
-            url: "https://picsum.photos/638/",
-            altText: "A lovely photo of downtown everywhereville.",
-            tag1: "inspirational",
-            tag2: "tangential"
-          },
-    item9: {
-            type: "image",
-            url: "https://picsum.photos/638/",
-            altText: "A lovely photo of downtown everywhereville.",
-            tag1: "inspirational",
-            tag2: "tangential"
-          },
-    item10: {
-            type: "poster",
-            text: "This is a poster block - whatcha think?",
-            background: "0f4c81",
-            orientation: "landscape",
-            tag1: "wild",
-            tag2: "wisdom"
-          },
 
-  };
+
+    */
+      const myData = {
+        'block' : {
+              'uniqueID' : '00',
+              'property1' : 'image',
+              'text' : 'I love NYC',
+              'property2' : 'https://images.unsplash.com/photo-1546436836-07a91091f160?ixlib',
+          },
+          'block' : {
+            'uniqueID' : '01',
+            'property1' : 'image',
+            'text' : 'I love nature',
+            'property2' : 'https://images.unsplash.com/photo-1620497998642-c89a1a70ebe0',
+        },
+        'block' : {
+            'uniqueID' : '02',
+            'property1' : 'image',
+            'text' : 'go away bad man',
+            'property2' : 'https://images.unsplash.com/photo-1529966464641-ccc3988e5e4e',
+        },
+    }
+        
+
+  /*function to iterate through the dataset and create a div based on "item" type*/
+
+
+
+/*for each item in the dataset create a new div and fill it with the item type.
+*/
+/*
+function itemGetter(dataset) {
+    const itemsHolder = []
+    for (let i=0; i < dataset.length; i++) {
+        let itemEach = dataset[i];
+        itemsHolder.push(itemEach)
+        console.log(itemEach);
+        return(itemEach);
+    }
+    console.log(itemsHolder);
+    return(itemsHolder);
+}
+itemGetter(["item1", "item2", "item3", "item4"])
+
+const itemNum = items.uniqueID;
+
+for (const key in itemNum) {
+    if (typeof items.itemNum[key] === "object") {
+        for (const nestedKey in items.itemNum[key]) {
+            console.log(itemNum[key][nestedKey]);
+        }
+    } else {
+        console.log(items[key]);
+    }
+}
+
+    console.log(`${key}: ${items[key]}`);
+    console.log(typeof items);
+    }
+    */
+    const li = document.createElement('li');
+    li.classList.add('item');
+    
+    /*document.querySelector('li').append(div);*/
+/*
+    function injectDiv(dataset) {
+        for (const key in dataset) {
+          if (dataset.hasOwnProperty(key)){
+            document.querySelector('ul').append(li);
+          }
+        }
+      }
+    
+    */
+
+      const p = {
+        'p0': 'value0',
+        'p1': {
+          'subDude': 'value1',
+        },
+        'p2': 'value2',
+        'p3': {
+          'subDude' : 'value3',
+          'subDude2' : 'artbitrary stuff'
+        }
+    };
+
+/*
+      let counter = 0;
+
+function deepIterator (target) {
+
+    if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key]);
+          }
+        } else {
+          console.log(target);
+        }
+      }
+      deepIterator(p)
+      */
+
+      const itemCount = myData.block
+
+      let count = 0;
+        for (const k in itemCount) {
+            if (itemCount.hasOwnProperty(k)) {
+                 count++;
+                 console.log(count);
+                 const li = document.createElement('li');
+                 li.classList.add(`item${count}`);
+                 document.querySelector('ul').append(li);
+            }
+            else {console.log()}
+        }
